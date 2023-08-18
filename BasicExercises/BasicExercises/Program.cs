42165c4e61c17fffe5e5858817034fe9ca2a3a5a
bogom1l <bogomil.stoev01@gmail.com>
2023-08-18 16:38:12 +0300

    few examples

--- BasicExercises/BasicExercises/Program.cs
+++ BasicExercises/BasicExercises/Program.cs
@@ -2,12 +2,47 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // OutExample();
+            // ParamsExample();
+        }
 
+        private static void OutExample()
+        {
+            Console.Write("Enter an integer: ");
+            var input = Console.ReadLine();
 
+            if (int.TryParse(input, out var parsedValue))
+            {
+                Console.WriteLine($"Parsed value: {parsedValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Invalid input. -> {parsedValue}");
+            }
+        }
 
+        private static void ParamsExample()
+        {
+            var result1 = Sum(1, 2, 3, 4, 5);
+            var result2 = Sum(10, 20);
+            var result3 = Sum(100, 200, 300, 1, 1, 1, 1);
+
+            Console.WriteLine($"Result 1: {result1}");
+            Console.WriteLine($"Result 2: {result2}");
+            Console.WriteLine($"Result 3: {result3}");
+        }
+
+        private static int Sum(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var num in numbers)
+            {
+                sum += num;
+            }
+
+            return sum;
         }
     }
 }

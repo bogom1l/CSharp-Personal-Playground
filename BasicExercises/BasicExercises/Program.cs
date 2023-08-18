c885d8e192451647022214d5dff1848c879062e6
bogom1l <bogomil.stoev01@gmail.com>
2023-08-18 15:31:07 +0300

    Initial push

---
+++ BasicExercises/BasicExercises/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicExercises
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+        }
+    }
+}

1535b9af54517081924c2278108021ef6c31e581
bogom1l <bogomil.stoev01@gmail.com>
2023-08-21 16:25:52 +0300

    initial name change

---
+++ OutRefExamples/OutRefExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OutRefExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

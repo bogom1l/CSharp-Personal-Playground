53174cc52082c77434a0bfa6b3ad75151b97a87a
bogom1l <bogomil.stoev01@gmail.com>
2023-08-21 16:29:28 +0300

    Renamed folders

--- OutRefExamples/OutRefExamples/Program.cs
+++ OutRefExamples/OutRefExamples/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine(123);
+            Console.WriteLine(321);
         }
     }
 }

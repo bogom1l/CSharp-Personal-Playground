a20859fc24344d54db69d93129e57430a039462b
bogom1l <bogomil.stoev01@gmail.com>
2023-08-21 18:07:05 +0300

    thread/task example

--- OutRefExamples/OutRefExamples/Program.cs
+++ OutRefExamples/OutRefExamples/Program.cs
@@ -1,11 +1,53 @@
 ﻿namespace OutRefExamples
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine(321);
+            int a = 10;
+            Console.WriteLine(a);
+
+            for (int i = 0; i < 10; i++)
+            {
+                RefIncrement(ref a);
+            }
+
+            InitializeNumberInMethod(out int b);
+
+            InitializeMultipleNumbersInMethod(out int c, out int d);
+
+            RefIncrement(ref c);
+
+            InitializeStringInMethod(out string text);
+        }
+
+        private static void RefIncrement(ref int number)
+        {
+            number += 5;
+            Console.WriteLine(number);
+        }
+
+        private static void InitializeNumberInMethod(out int number)
+        {
+            number = 99;
+            Console.WriteLine(number);
+            Console.WriteLine("---");
+        }
+
+        private static void InitializeMultipleNumbersInMethod(out int c, out int d)
+        {
+            c = 404;
+            d = 505;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine("---");
+        }
+
+        private static void InitializeStringInMethod(out string text)
+        {
+            text = "NewText";
+            Console.WriteLine(text);
+            Console.WriteLine("---");
         }
     }
 }
---
+++ ThreadLockExample/ThreadLockExample/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ThreadTaskExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main Thread";
+
+            Thread thread1 = new Thread(() =>
+            {
+                CountUp();
+            });
+
+            Thread thread2 = new Thread(() =>
+            {
+                CountDown();
+            });
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.WriteLine(mainThread.Name + " is complete!");
+            Console.ReadKey();
+        }
+
+        public static void CountUp()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Timer #1: {i} seconds");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine($"Timer #1 is complete!");
+        }
+
+        public static void CountDown()
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"Timer #2: {i} seconds");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine($"Timer #2 is complete!");
+        }
+
+        private static int SumNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
+/*
+   Task task = new Task(() =>
+   {
+       Console.WriteLine("Running task in separate thread...");
+       int result = SumNumbers(1, 2);
+       Console.WriteLine("Result of addition: " + result);
+   });
+
+   task.Start();
+
+   Console.WriteLine("Main thread is done");
+   Console.ReadLine();
+*/

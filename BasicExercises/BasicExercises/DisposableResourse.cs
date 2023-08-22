b1c82ee05d5037d1873f7b94da926c6a36e76e7d
bogom1l <bogomil.stoev01@gmail.com>
2023-08-22 11:42:16 +0300

    added Using example

---
+++ BasicExercises/BasicExercises/DisposableResourse.cs
@@ -0,0 +1,18 @@
+﻿namespace BasicExercises
+{
+    using System;
+
+    public class DisposableResource : IDisposable
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("DisposableResource is doing something.");
+        }
+
+        public void Dispose()
+        {
+            // Perform cleanup operations here
+            Console.WriteLine("DisposableResource is being disposed.");
+        }
+    }
+}
--- BasicExercises/BasicExercises/Program.cs
+++ BasicExercises/BasicExercises/Program.cs
@@ -7,7 +7,7 @@
             // OutExample();
             // ParamsExample();
 
-	//TODO: algos
+            UsingExample();
         }
 
         private static void OutExample()
@@ -45,6 +45,32 @@
             }
 
             return sum;
+        }
+
+        private static void UsingExample()
+        {
+            // Example 1: Using with a single object
+            using (DisposableResource resource = new DisposableResource())
+            {
+                resource.DoSomething();
+            } // The Dispose method of DisposableResource is automatically called here
+
+            // Example 2: Using with multiple objects
+            using (DisposableResource resource1 = new DisposableResource())
+            using (DisposableResource resource2 = new DisposableResource())
+            {
+                resource1.DoSomething();
+                resource2.DoSomething();
+            } // Dispose methods of resource1 and resource2 are automatically called here
+
+            // Example 3: Using with FileStream
+            string filePath = "example.txt";
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                byte[] data = System.Text.Encoding.UTF8.GetBytes("Hello, using statement!");
+                fileStream.Write(data, 0, data.Length);
+            } // The file stream is automatically closed here
+
         }
     }
 }

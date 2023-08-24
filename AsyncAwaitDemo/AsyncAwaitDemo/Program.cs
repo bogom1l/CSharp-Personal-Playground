de53c503bf0362284868c80ca31f03fdd84a5079
bogom1l <bogomil.stoev01@gmail.com>
2023-08-24 11:33:30 +0300

    add async demo

---
+++ AsyncAwaitDemo/AsyncAwaitDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AsyncAwaitDemo
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Ivaylo Kenov - Asynchronous Processing Lecture:
+            //https://www.youtube.com/live/m5-hYdj40So?si=0qPraHJR8SA3jOUE
+
+            await AsyncCooker.Work();
+        }
+    }
+}

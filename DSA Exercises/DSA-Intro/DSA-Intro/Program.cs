055d2cde821980484b6eefe771a855fd01fb16ac
bogom1l <bogomil.stoev01@gmail.com>
2023-09-01 15:30:37 +0300

    SortedSet example

--- DSA Exercises/DSA-Intro/DSA-Intro/Program.cs
+++ DSA Exercises/DSA-Intro/DSA-Intro/Program.cs
@@ -1,11 +1,22 @@
 ﻿namespace DSA_Intro
 {
-    public class Program
+    public class Program //TODO: Ctrl + Alt + '
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine(123);
+            SortedSet<int> mySortedSet = new();
+
+            Random random = new();
+
+            for (var i = 0; i < 10; i++)
+            {
+                mySortedSet.Add(random.Next(1, 99));
+            }
+
+            foreach (var i in mySortedSet)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 }

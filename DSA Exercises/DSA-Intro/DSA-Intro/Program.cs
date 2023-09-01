235f32ead51db5e468cc79ac70acd676b7471b9b
bogom1l <bogomil.stoev01@gmail.com>
2023-09-01 14:58:05 +0300

    Relocated Folders

---
+++ DSA Exercises/DSA-Intro/DSA-Intro/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DSA_Intro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(123);
+        }
+    }
+}

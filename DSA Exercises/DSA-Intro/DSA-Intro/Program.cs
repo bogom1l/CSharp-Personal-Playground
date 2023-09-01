747fb3b0dabdd005d1e3d973432f57a0b97cfeab
bogom1l <bogomil.stoev01@gmail.com>
2023-09-01 17:39:36 +0300

    LongestCommonSubsequence

--- DSA Exercises/DSA-Intro/DSA-Intro/Program.cs
+++ DSA Exercises/DSA-Intro/DSA-Intro/Program.cs
@@ -4,19 +4,62 @@
     {
         private static void Main(string[] args)
         {
-            SortedSet<int> mySortedSet = new();
+            string str1 = "abcdaf";
+            string str2 = "acbcf";
 
-            Random random = new();
+            int lcsRecursion = LCS(str1, str2, str1.Length, str2.Length);
+            Console.WriteLine("LCS (Recursion): " + lcsRecursion);
 
-            for (var i = 0; i < 10; i++)
+            int lcsDynamic = LCS_Dynamic(str1, str2);
+            Console.WriteLine("LCS (Dynamic): " + lcsDynamic);
+        }
+
+        //LongestCommonSubsequence - Recursion
+        private static int LCS(string str1, string str2, int i, int j)
+        {
+            if (i == 0 || j == 0)
             {
-                mySortedSet.Add(random.Next(1, 99));
+                return 0;
             }
 
-            foreach (var i in mySortedSet)
+            if (str1[i - 1] == str2[j - 1])
             {
-                Console.Write(i + " ");
+                return 1 + LCS(str1, str2, i - 1, j - 1);
             }
+
+            return Math.Max(LCS(str1, str2, i, j - 1), LCS(str1, str2, i - 1, j));
+        }
+
+        //LongestCommonSubsequence - Dynamic Solution
+        private static int LCS_Dynamic(string str1, string str2)
+        {
+            int m = str1.Length;
+            int n = str2.Length;
+
+            int[,] temp = new int[m + 1, n + 1];
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        temp[i, j] = 0;
+                    }
+                    else if (str1[i - 1] == str2[j - 1])
+                    {
+                        int currValue1 = temp[i - 1, j - 1] + 1;
+                        temp[i, j] = currValue1;
+                    }
+                    else
+                    {
+                        int currValue2 = Math.Max(temp[i - 1, j], temp[i, j - 1]);
+                        temp[i, j] = currValue2;
+                    }
+                }
+            }
+
+            return temp[m, n];
         }
     }
 }

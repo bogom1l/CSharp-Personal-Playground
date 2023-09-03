17dcd217f876918a04477952afbd4c58b0ced9bf
bogom1l <bogomil.stoev01@gmail.com>
2023-09-03 17:39:03 +0300

    binary tree

---
+++ DSA Exercises/TreesAndTreeTraversalAlgorithmsDemo/TreesAndTreeTraversalAlgorithmsDemo/BinaryTree.cs
@@ -0,0 +1,53 @@
+﻿namespace TreesAndTreeTraversalAlgorithmsDemo
+{
+    public class BinaryTree
+    {
+        public Node Root;
+
+        public BinaryTree()
+        {
+            this.Root = null;
+        }
+
+        public void Insert(int data)
+        {
+            this.Root = InsertRecursive(this.Root, data);
+        }
+
+        private Node InsertRecursive(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return root;
+            }
+
+            if (data < root.Data)
+            {
+                root.Left = InsertRecursive(root.Left, data);
+            }
+            else if (data > root.Data)
+            {
+                root.Right = InsertRecursive(root.Right, data);
+            }
+
+            return root;
+        }
+
+        // Printing the nodes in ascending order
+        public void PrintInorder()
+        {
+            InorderRecursive(this.Root);
+        }
+
+        private void InorderRecursive(Node root)
+        {
+            if (root != null)
+            {
+                InorderRecursive(root.Left);
+                Console.Write(root.Data + " ");
+                InorderRecursive(root.Right);
+            }
+        }
+    }
+}
---
+++ DSA Exercises/TreesAndTreeTraversalAlgorithmsDemo/TreesAndTreeTraversalAlgorithmsDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TreesAndTreeTraversalAlgorithmsDemo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+
+            tree.Insert(50);
+            tree.Insert(30);
+            tree.Insert(20);
+            tree.Insert(40);
+            tree.Insert(70);
+            tree.Insert(60);
+            tree.Insert(80);
+
+            Console.WriteLine("Inorder traversal of binary tree: ");
+            tree.PrintInorder();
+        }
+    }
+}

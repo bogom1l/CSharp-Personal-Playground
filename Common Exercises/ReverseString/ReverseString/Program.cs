a37f5b9360d93822dc9eb5cb7f79e956c5c773b4
bogom1l <bogomil.stoev01@gmail.com>
2023-09-04 18:48:13 +0300

    simple solution - reversing string

---
+++ Common Exercises/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("Hello World"));
+        }
+
+        private static string ReverseString(string str)
+        {
+            string reversedString = string.Empty;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversedString += str[i];
+            }
+
+            return reversedString;
+        }
+    }
+}

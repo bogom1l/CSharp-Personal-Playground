b34e297d7065b284aa7bcd898fcb967f2d180c63
bogom1l <bogomil.stoev01@gmail.com>
2023-09-07 16:18:31 +0300

    simple example added

--- DSA Exercises/RecursionDemo/RecursionDemo/Program.cs
+++ DSA Exercises/RecursionDemo/RecursionDemo/Program.cs
@@ -5,9 +5,12 @@
         private static void Main()
         {
             // FindMaxValueRecursion
-            List<int> myList = new List<int> { 4, 64, 234, 300, 32 };
-            int max = FindMaxValueRecursion(myList);
-            Console.WriteLine(max);
+            // List<int> myList = new List<int> { 4, 64, 234, 300, 32 };
+            // int max = FindMaxValueRecursion(myList);
+            // Console.WriteLine(max);
+
+            // Walk
+            Walk(15);
         }
 
         private static int FindMaxValueRecursion(List<int> myList)
@@ -29,6 +32,17 @@
             int maxInRest = FindMaxValueRecursion(restOfList);
 
             return Math.Max(possibleMax, maxInRest);
+        }
+
+        private static void Walk(int steps)
+        {
+            if (steps == 0) // dyno na rekursiqta
+            {
+                return;
+            }
+
+            Console.WriteLine($"You took a step!  {steps}");
+            Walk(steps - 1);
         }
     }
 }

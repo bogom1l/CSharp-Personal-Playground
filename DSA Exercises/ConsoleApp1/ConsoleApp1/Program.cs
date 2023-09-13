0c3153b39f7f64a3c324383adf1fe8653dc15412
bogom1l <bogomil.stoev01@gmail.com>
2023-09-13 16:22:48 +0300

    testing some extraordinary features

---
+++ DSA Exercises/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4};
+
+            try
+            {
+                Console.WriteLine(arr[10]);
+            }
+            finally
+            {
+                Console.WriteLine("asd");
+            }
+        }
+    }
+
+    enum Color
+    {
+        Red,
+        Green = 0,
+        Blue = 0
+    }
+}

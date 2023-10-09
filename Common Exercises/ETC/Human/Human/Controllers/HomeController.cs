792a28f714080e95bddecac3acf74d98e1f3779a
bogom1l <bogomil.stoev01@gmail.com>
2023-10-09 16:45:53 +0300

    Trying out

---
+++ Common Exercises/ETC/Human/Human/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿namespace Human.Controllers
+{
+    using Human.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<int> numbers = Enumerable.Range(1, 10);
+
+            return View(numbers);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

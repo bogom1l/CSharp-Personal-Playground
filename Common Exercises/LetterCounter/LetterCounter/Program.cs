6cda9fa3b63e39f5b7a49d75235932a59ff3b619
bogom1l <bogomil.stoev01@gmail.com>
2024-01-28 15:32:49 +0200

    simple letter counter
    for my usage

---
+++ Common Exercises/LetterCounter/LetterCounter/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LetterCounter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //string sentence = "технология за транслация на адреси";
+            string sentence = "tehnologiq za translaciq na adresi";
+            Console.WriteLine("all letters = " + sentence.Length);
+
+            Dictionary<char, int> uniqueSymbols = new Dictionary<char, int>();
+
+            foreach (var c in sentence)
+            {
+                if (uniqueSymbols.ContainsKey(c))
+                {
+                    uniqueSymbols[c]++;
+                }
+                else
+                {
+                    uniqueSymbols[c] = 1;
+                }
+            }
+
+            Console.WriteLine($"unique symbols = {uniqueSymbols.Keys.Count}");
+
+            foreach (var uniqueSymbol in uniqueSymbols)
+            {
+                Console.WriteLine($"{uniqueSymbol.Key} - {uniqueSymbol.Value}");
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            var sortedSymbols = uniqueSymbols.OrderByDescending(pair => pair.Value);
+
+            foreach (var uniqueSymbol in sortedSymbols)
+            {
+                Console.WriteLine($"{uniqueSymbol.Key} - {uniqueSymbol.Value}");
+            }
+        }
+    }
+}

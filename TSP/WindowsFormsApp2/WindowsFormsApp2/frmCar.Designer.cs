f406e4e83814a0abf31fbc03348ba7264bcc3341
bogom1l <bogomil.stoev01@gmail.com>
2024-05-14 09:42:11 +0300

    initial commit

---
+++ TSP/WindowsFormsApp2/WindowsFormsApp2/frmCar.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class frmCar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtLicensePlate = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPriceForRepairing = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtSeats = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(589, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(904, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(278, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(866, 318);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // txtLicensePlate
+            // 
+            this.txtLicensePlate.Location = new System.Drawing.Point(103, 47);
+            this.txtLicensePlate.Name = "txtLicensePlate";
+            this.txtLicensePlate.Size = new System.Drawing.Size(137, 20);
+            this.txtLicensePlate.TabIndex = 4;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(103, 88);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(137, 20);
+            this.txtModel.TabIndex = 5;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(103, 127);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(137, 20);
+            this.txtMake.TabIndex = 6;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(103, 171);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(137, 20);
+            this.txtColor.TabIndex = 7;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(103, 209);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(141, 20);
+            this.txtYear.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(105, 334);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 31);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Empty All Fields";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "License Plate:";
+            // 
+            // txtPriceForRepairing
+            // 
+            this.txtPriceForRepairing.Location = new System.Drawing.Point(105, 287);
+            this.txtPriceForRepairing.Name = "txtPriceForRepairing";
+            this.txtPriceForRepairing.Size = new System.Drawing.Size(139, 20);
+            this.txtPriceForRepairing.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Make";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Year";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Seats";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 290);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Price For Repair";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtSeats
+            // 
+            this.txtSeats.Location = new System.Drawing.Point(103, 251);
+            this.txtSeats.Name = "txtSeats";
+            this.txtSeats.Size = new System.Drawing.Size(137, 20);
+            this.txtSeats.TabIndex = 21;
+            // 
+            // frmCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 481);
+            this.Controls.Add(this.txtSeats);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPriceForRepairing);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtLicensePlate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmCar";
+            this.Text = "frmCar";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtLicensePlate;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPriceForRepairing;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtSeats;
+    }
+}
---
+++ TSP/WindowsFormsApp2/WindowsFormsApp2/frmRepair.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+    using Database;
+    using Entities;
+
+    public partial class frmRepair : Form
+    {
+        private readonly DataAccess dataAccess = new DataAccess();
+
+        public frmRepair()
+        {
+            InitializeComponent();
+            DisplayAllRepairs();
+        }
+
+
+        // Create
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtCarId.Text == "" || txtClientId.Text == "" ||
+                txtStartDate.Text == "" ||
+                txtEndDate.Text == "")
+            {
+                MessageBox.Show("Please fill all the fields to create a repair.");
+            }
+            else
+            {
+                try
+                {
+                    int carId = Convert.ToInt32(txtCarId.Text);
+                    int clientId = Convert.ToInt32(txtClientId.Text);
+
+                    if (!dataAccess.CarExists(carId))
+                    {
+                        MessageBox.Show("The specified CarId does not exist. Please enter a valid CarId.");
+                        return;
+                    }
+
+                    if (!dataAccess.ClientExists(clientId))
+                    {
+                        MessageBox.Show("The specified ClientId does not exist. Please enter a valid ClientId.");
+                        return;
+                    }
+
+                    Repair newRepair = new Repair
+                    {
+                        CarId = Convert.ToInt32(txtCarId.Text),
+                        ClientId = Convert.ToInt32(txtClientId.Text),
+                        DateOfStartingRepair = txtStartDate.Text,
+                        DateOfFinishingRepair = txtEndDate.Text,
+                        IsPaid = checkBoxIsPaid.Checked,
+                        IsReturned = checkBoxIsReturned.Checked
+                    };
+
+                    dataAccess.CreateRepair(newRepair);
+                    DisplayAllRepairs();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+        }
+
+        // Update
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                try
+                {
+                    int repairId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+                    Repair repairToUpdate = dataAccess.GetAllRepairs().First(x => x.Id == repairId);
+
+                    if (txtCarId.Text.Length != 0)
+                    {
+                        repairToUpdate.CarId = Convert.ToInt32(txtCarId.Text);
+                    }
+
+                    if (txtClientId.Text.Length != 0)
+                    {
+                        repairToUpdate.ClientId = Convert.ToInt32(txtClientId.Text);
+                    }
+
+                    if (txtStartDate.Text.Length != 0)
+                    {
+                        repairToUpdate.DateOfStartingRepair = txtStartDate.Text;
+                    }
+
+                    if (txtEndDate.Text.Length != 0)
+                    {
+                        repairToUpdate.DateOfFinishingRepair = txtEndDate.Text;
+                    }
+
+                    repairToUpdate.IsPaid = checkBoxIsPaid.Checked;
+                    repairToUpdate.IsReturned = checkBoxIsReturned.Checked;
+
+                    dataAccess.UpdateRepair(repairToUpdate);
+                    DisplayAllRepairs();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a repair to update.");
+            }
+        }
+
+        // Delete
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to delete this repair?", "Confirm",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    int repairIdToDelete = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+                    dataAccess.DeleteRepair(repairIdToDelete);
+
+                    DisplayAllRepairs();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a repair to delete.");
+            }
+        }
+
+
+        // Empty all fields
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtCarId.Text = "";
+            txtClientId.Text = "";
+            txtStartDate.Text = "";
+            txtEndDate.Text = "";
+            checkBoxIsPaid.Checked = false;
+            checkBoxIsReturned.Checked = false;
+        }
+
+
+        // Display all repairs
+        private void DisplayAllRepairs()
+        {
+            List<Repair> allRepairs = dataAccess.GetAllRepairs();
+            dataGridView1.DataSource = allRepairs;
+        }
+    }
+}
